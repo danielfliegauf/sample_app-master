b12e5e3837f2e5b0932aaf14754216af1adb3682
Daniel Fliegauf <daniel.fliegauf@gmail.com>
2013-01-22 00:42:29 +0100

    changed privacy and agb link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
     if session[:mobile_param]
       session[:mobile_param] == "1"
     else
-      request.user_agent =~ /Mobile|webOS|iphone|android/
+      request.user_agent =~ /Mobile|webOS|iphone|android|bada|mobile|samsung|symbian/
     end
   end
 
