50da08f6c4d72d9bfc982c2ad6c66d67cbe0fad9
Daniel Fliegauf <daniel.fliegauf@gmail.com>
2013-02-25 15:46:09 +0100

    facebook display changed into touch

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '147347702004703', '92909b6e4217f41cac177c809804424f',
   # {:scope => 'email, birthday', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
-  {:scope => 'email, publish_stream, user_groups', :display => "popup", :client_options => {:ssl => {:verify => false}}} #TODO:remove :client_options on production!!!
+  {:scope => 'email, publish_stream, user_groups', :display => "touch", :client_options => {:ssl => {:verify => false}}} #TODO:remove :client_options on production!!!
 end
