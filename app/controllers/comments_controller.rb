90a87b2a738e1e1c4fe140eaf964fc01ab1a4519
Daniel Fliegauf <daniel.fliegauf@gmail.com>
2013-04-21 21:26:03 +0200

    comment added main alter domain login

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,29 @@
 class CommentsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :edit, :destroy]
 
+
   def create
+    session[:return_to] = params[:return_to]
     @micropost = Micropost.find(params[:micropost_id])
     @comment = @micropost.comments.build(params[:comment])
     @comment.user = current_user
     @comment.read = false
     if @comment.save
       #sende Mail bei neuem Comment an Microposterteller
-      UserMailer.comment_email(@micropost, @comment).deliver
+      UserMailer.comment_email(@micropost, @comment, current_user).deliver
     	flash[:success] = "Comment was created successfully!"
     else
     	flash[:error] = "There was an Error!"
     end
-    redirect_to micropost_path(@micropost)
+    redirect_back_or(micropost_path(@micropost))
   end
 
   def destroy
+    session[:return_to] = params[:return_to]
   	@micropost = Micropost.find(params[:micropost_id])
   	@comment = @micropost.comments.find(params[:id])
   	@comment.destroy
-  	redirect_to micropost_path(@micropost)
+  	redirect_back_or(micropost_path(@micropost))
   end
 
   def show
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -17,32 +17,32 @@ class UserMailer < ActionMailer::Base
 
   	mail(:to => user.email,
          :subject => "Willkommen bei Connectify",
-         :body => "Hallo "+user.name+"! Du hast dich erfolgreich bei Connectify eingeloggt und kannst ab sofort loslegen und dein erstes Micropost schreiben. Du weisst nicht was das ist? Dann lies dir die Einfuehrung durch! Es ist ganz einfach. Viel Spass dabei!")
+         :body => "Hallo "+user.name+"! Du hast dich erfolgreich bei Connectify eingeloggt und kannst ab sofort loslegen und dein erste Activity erstellen. Du weisst nicht was das ist? Dann lies dir die Einfuehrung durch! Es ist ganz einfach. Viel Spass dabei!")
   end
 
-  def comment_email(micropost, comment)
+  def comment_email(micropost, comment, writer)
   	# @user = user
   	@micropost = micropost
   	mail(
       :to => micropost.user.email, 
-      :subject => "Connectify - Dein Micropost hat einen neuen Kommentar",
-      :body => "Hallo "+micropost.user.name+"! "+comment.body
+      :subject => "Connectify - Deine Activity hat einen neuen Kommentar",
+      :body => "Hallo #{micropost.user.name.split[0]}!\n\n#{writer.name} hat einen neuen Kommentar hinterlassen:\n\n '#{comment.body}'\n\n Klicke hier um direkt hinzugelangen: http://connectify.de/microposts/#{micropost.id}\n\n Viel Spass weiterhin auf Connectify!"
     )
 
-    micropost.user.followers.each do |user|
-      mail(
-        :to => user.email,
-        :subject => "Connectify - Ein Micropost hat ein neuen Kommentar",
-        :body => "Hallo "+user.name+"! "+comment.body
-      )
-    end
+    # micropost.user.followers.each do |user|
+    #   mail(
+    #     :to => user.email,
+    #     :subject => "Connectify - Eine Activity hat einen neuen Kommentar",
+    #     :body => "Hallo "+user.name+"! "+comment.body
+    #   )
+    # end
 
-    micropost.mindposts.each do |mindpost|
-      mail(
-        :to => mindpost.user.email,
-        :subject => "Connectify - Ein Micropost hat ein neuen Kommentar",
-        :body => "Hallo "+mindpost.user.name+"! "+comment.body
-      )
-    end
+    # micropost.mindposts.each do |mindpost|
+    #   mail(
+    #     :to => mindpost.user.email,
+    #     :subject => "Connectify - Ein Micropost hat ein neuen Kommentar",
+    #     :body => "Hallo "+mindpost.user.name+"! "+comment.body
+    #   )
+    # end
   end
 end
