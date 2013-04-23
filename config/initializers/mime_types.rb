4a49facf0c93798a8b308a5d1b476bf803b29ead
Daniel Fliegauf <daniel.fliegauf@gmail.com>
2013-04-23 16:00:06 +0200

    added font MIME types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,10 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 Mime::Type.register_alias "text/html", :mobile
+
+# tell Rack (and Sprockets) about modern font MIME types:
+Rack::Mime::MIME_TYPES['.woff'] = 'application/x-font-woff'
+
+Rack::Mime::MIME_TYPES['.ttf'] = 'font/truetype'
+Rack::Mime::MIME_TYPES['.otf'] = 'font/opentype'
+Rack::Mime::MIME_TYPES['.eot'] = 'application/vnd.ms-fontobject'
